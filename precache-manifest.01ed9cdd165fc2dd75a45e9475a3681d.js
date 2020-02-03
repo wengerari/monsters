753853f65d1a346c35f6a75164f9e818ac17aeda
wengerari <wengerari@gmail.com>
2020-02-03 21:54:57 +0300

    Updates

---
+++ precache-manifest.01ed9cdd165fc2dd75a45e9475a3681d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7dda13ef746bb9123230024910181c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6b1a3a252741ebafcced",
+    "url": "/monsters/static/css/main.b112c536.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1a3a252741ebafcced",
+    "url": "/monsters/static/js/main.7c669c00.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
